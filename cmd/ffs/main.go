7a2d7394df0e624d82985913b290e36e9579ab15
Jacob Moody <jsmoody@iastate.edu>
2019-05-09 10:42:46 -0500

    Added diskfs example Removed test fs from main.go

--- cmd/ffs/main.go
+++ cmd/ffs/main.go
@@ -1,61 +1,25 @@
 package main
 
 import (
-	"aqwari.net/net/styx"
-	"errors"
-	"github.com/majiru/ffs"
-	"github.com/majiru/ffs/fs/domainfs"
-	"github.com/majiru/ffs/fs/pastefs"
-	"github.com/majiru/ffs/pkg/fsutil"
-	"github.com/majiru/ffs/pkg/server"
-	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"aqwari.net/net/styx"
+	"github.com/majiru/ffs"
+	"github.com/majiru/ffs/fs/diskfs"
+	"github.com/majiru/ffs/fs/domainfs"
+	"github.com/majiru/ffs/fs/pastefs"
+	"github.com/majiru/ffs/pkg/server"
 )
-
-type chrisfs struct {
-	file ffs.Writer
-	root *fsutil.Dir
-}
-
-func (fs *chrisfs) ReadDir(path string) (ffs.Dir, error) {
-	dir := *fs.root
-	return &dir, nil
-}
-
-func (fs *chrisfs) Open(path string, mode int) (interface{}, error) {
-	if mode&os.O_TRUNC != 0 {
-		if err := fs.file.Truncate(1); err != nil {
-			return nil, errors.New("chrisfs.Open: Truncate Failed")
-		}
-		fs.file.Seek(0, io.SeekStart)
-	}
-	return fs.file, nil
-}
-
-func (fs *chrisfs) Stat(path string) (os.FileInfo, error) {
-	switch path {
-	case "/":
-		return fs.root.Stat()
-	case "/index.html":
-		return fs.file.Stat()
-	}
-
-	log.Println(path, " not found")
-	return nil, os.ErrNotExist
-}
 
 func main() {
 	var styxServer styx.Server
 	styxServer.TraceLog = log.New(os.Stderr, "", 0)
 	styxServer.ErrorLog = log.New(os.Stderr, "", 0)
-	file := fsutil.CreateFile([]byte("Hello World!\n"), 0777, "index.html")
-	fi, _ := file.Stat()
-	dir := fsutil.CreateDir("/", fi)
 
 	m := make(map[string]ffs.Fs)
-	m["127.0.0.1"] = &chrisfs{file, dir}
+	m["127.0.0.1"] = &diskfs.Diskfs{"/tmp"}
 	m["localhost"] = pastefs.NewPastefs()
 	dfs := &domainfs.Domainfs{m}
 
---
+++ fs/diskfs/diskfs.go
@@ -0,0 +1,24 @@
+package diskfs
+
+import (
+	"os"
+	"path"
+
+	"github.com/majiru/ffs"
+)
+
+type Diskfs struct {
+	Root string
+}
+
+func (fs *Diskfs) Stat(name string) (os.FileInfo, error) {
+	return os.Stat(path.Join(fs.Root, name))
+}
+
+func (fs *Diskfs) ReadDir(name string) (ffs.Dir, error) {
+	return os.Open(path.Join(fs.Root, name))
+}
+
+func (fs *Diskfs) Open(name string, mode int) (file interface{}, err error) {
+	return os.Open(path.Join(fs.Root, name))
+}
