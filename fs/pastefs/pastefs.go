e321df507d48ebb5aa3b9715be4ca9f02360effe
Jacob Moody <jsmoody@iastate.edu>
2019-05-08 21:20:50 -0500

    Fixed up pastefs, added index.html landing page

--- fs/pastefs/pastefs.go
+++ fs/pastefs/pastefs.go
@@ -1,19 +1,30 @@
 package pastefs
 
 import (
-	"os"
-	"path"
-	"log"
-	"time"
-	"strconv"
 	"github.com/majiru/ffs"
 	"github.com/majiru/ffs/pkg/fsutil"
+	"html/template"
+	"io"
+	"os"
+	"path"
+	"strconv"
+	"time"
 )
 
-
 type Pastefs struct {
-	newpaste	*fsutil.File
-	pastes		*fsutil.Dir
+	newpaste *fsutil.File
+	pastes   *fsutil.Dir
+}
+
+func dir2html(w io.Writer, dir *fsutil.Dir) (err error) {
+	content := struct{ Files []os.FileInfo }{dir.Dup()}
+	t := template.New("homepage")
+	t, err = t.Parse(homepage)
+	if err != nil {
+		return
+	}
+	err = t.ExecuteTemplate(w, "homepage", content)
+	return
 }
 
 func (fs *Pastefs) root() ffs.Dir {
@@ -26,7 +37,7 @@ func (fs Pastefs) Stat(file string) (os.FileInfo, error) {
 	if file == "/" {
 		return fs.root().Stat()
 	}
-	if file == "/new" {
+	if file == "/new" || file == "/index.html" {
 		return fs.newpaste.Stat()
 	}
 	if file == "/pastes" {
@@ -48,20 +59,40 @@ func (fs Pastefs) ReadDir(path string) (ffs.Dir, error) {
 }
 
 func (fs Pastefs) Open(file string, mode int) (interface{}, error) {
-	//User is doing a write, we don't care where, this results in a paste
-	if file == "/new" {
-		name := strconv.FormatInt(time.Now().Unix(), 10)
-		log.Println("Doing Write to: ", name)
-		f := fsutil.CreateFile([]byte("\n"), 0777, name)
-		fi, _ := f.Stat()
-		fs.pastes.Append(fi)
-		return f, nil
+	if file == "/index.html" {
+		f := fsutil.CreateFile([]byte(""), 0644, "/index.html")
+		err := dir2html(f, fs.pastes)
+		return f, err
 	}
-
-	f, _ := fs.pastes.Find(path.Base(file))
-	return f.Sys(), nil
+	if file == "/new" {
+		if mode&os.O_RDWR != 0 || mode&os.O_WRONLY != 0 {
+			name := strconv.FormatInt(time.Now().Unix(), 10)
+			f := fsutil.CreateFile([]byte(""), 0777, name)
+			fi, _ := f.Stat()
+			fs.pastes.Append(fi)
+			return f, nil
+		}
+	}
+	if f, err := fs.pastes.Find(path.Base(file)); err == nil {
+		return f.Sys(), nil
+	}
+	return nil, os.ErrNotExist
 }
 
 func NewPastefs() *Pastefs {
 	return &Pastefs{fsutil.CreateFile([]byte("\n"), 0777, "new"), fsutil.CreateDir("pastes")}
 }
+
+const homepage = `
+<!DOCTYPE HTML>
+<head>
+	<title>PasteFS</title>
+</head>
+<body>
+	<h1>Paste FS</h1><br><br>
+	<p>Recent Pastes:</p><br>
+	{{ range .Files }}
+	<a href="/pastes/{{ .Name }}">{{ .Name }}</a><br>
+	{{end}}
+</body>
+`
