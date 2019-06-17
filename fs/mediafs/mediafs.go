d7d2503e92433e56d3b58cb2e41f58e07caffed4
Jacob Moody <jsmoody@iastate.edu>
2019-06-17 18:43:20 -0500

    Rework dbfile to make use of chanfile for updates on close when a write occured

--- fs/mediafs/mediafs.go
+++ fs/mediafs/mediafs.go
@@ -5,21 +5,22 @@ import (
 	"html/template"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"sync"
 
 	anidb "github.com/majiru/anidb2json"
 	"github.com/majiru/ffs"
+	"github.com/majiru/ffs/pkg/chanfile"
 	"github.com/majiru/ffs/pkg/fsutil"
 )
 
 type Mediafs struct {
 	*sync.RWMutex
-	Root             *fsutil.Dir
-	DB               *anidb.TitleDB
-	dbfile, homepage *fsutil.File
+	Root     *fsutil.Dir
+	DB       *anidb.TitleDB
+	dbfile   *chanfile.File
+	homepage *fsutil.File
 }
 
 func NewMediafs(db io.ReadWriter) (fs *Mediafs, err error) {
@@ -27,22 +28,22 @@ func NewMediafs(db io.ReadWriter) (fs *Mediafs, err error) {
 		&sync.RWMutex{},
 		fsutil.CreateDir("/"),
 		&anidb.TitleDB{},
-		fsutil.CreateFile([]byte(""), 0644, "db"),
+		chanfile.CreateFile([]byte(""), 0644, "db"),
 		fsutil.CreateFile([]byte(""), 0644, "index.html"),
 	}
 	if db != nil {
-		_, err = io.Copy(fs.dbfile, db)
+		_, err = io.Copy(fs.dbfile.Content, db)
 		if err != nil {
 			return
 		}
-		fs.dbfile.Seek(0, io.SeekStart)
+		fs.dbfile.Content.Seek(0, io.SeekStart)
 		err = fs.update()
 	}
+	go fs.dbproc()
 	return
 }
 
 func (fs *Mediafs) updateTree() {
-	fs.Lock()
 	fs.Root = fsutil.CreateDir("/")
 	for _, s := range fs.DB.Anime {
 		subdir := fsutil.CreateDir(s.Name)
@@ -51,12 +52,9 @@ func (fs *Mediafs) updateTree() {
 		}
 		fs.Root.Append(subdir.Stats)
 	}
-	fs.Unlock()
 }
 
 func (fs *Mediafs) updateHomepage() (err error) {
-	fs.Lock()
-	defer fs.Unlock()
 	t := template.New("homepage")
 	t.Funcs(template.FuncMap{
 		"files": func(name string) []os.FileInfo {
@@ -77,37 +75,53 @@ func (fs *Mediafs) updateHomepage() (err error) {
 }
 
 func (fs *Mediafs) updateDB() (err error) {
-	fs.Lock()
-	b, err := ioutil.ReadAll(fs.dbfile)
+	b, err := ioutil.ReadAll(fs.dbfile.Content)
 	if err != nil {
 		return
 	}
-	fs.dbfile.Seek(0, io.SeekStart)
+	fs.dbfile.Content.Seek(0, io.SeekStart)
 	err = json.Unmarshal(b, fs.DB)
-	fs.Unlock()
 	return
 }
 
 func (fs *Mediafs) update() (err error) {
+	fs.Lock()
 	if err = fs.updateDB(); err != nil {
+		fs.Unlock()
 		return
 	}
 	fs.updateTree()
-	return fs.updateHomepage()
+	err = fs.updateHomepage()
+	fs.Unlock()
+	return
 }
 
 func (fs *Mediafs) Check() (err error) {
-	if fs.dbfile.Stats.ModTime().After(fs.homepage.Stats.ModTime()) {
+	if fs.dbfile.Content.Stats.ModTime().After(fs.homepage.Stats.ModTime()) {
 		return fs.update()
 	}
 	return
 }
 
-func (fs *Mediafs) Stat(file string) (os.FileInfo, error) {
-	if err := fs.Check(); err != nil {
-		log.Println(err)
-		return nil, err
+func (fs *Mediafs) dbproc() {
+	for {
+		m := <-fs.dbfile.Req
+		switch m.Type {
+		case chanfile.Read:
+			fs.dbfile.Recv <- chanfile.RecvMsg{chanfile.Commit, nil}
+		case chanfile.Write:
+			fs.dbfile.Recv <- chanfile.RecvMsg{chanfile.Commit, nil}
+		case chanfile.Trunc:
+			fs.dbfile.Recv <- chanfile.RecvMsg{chanfile.Commit, nil}
+		case chanfile.Close:
+			fs.dbfile.Recv <- chanfile.RecvMsg{chanfile.Commit, fs.Check()}
+		}
 	}
+}
+
+func (fs *Mediafs) Stat(file string) (os.FileInfo, error) {
+	fs.RLock()
+	defer fs.RUnlock()
 	switch file {
 	case "/index.html":
 		return fs.homepage.Stat()
@@ -121,10 +135,8 @@ func (fs *Mediafs) Stat(file string) (os.FileInfo, error) {
 }
 
 func (fs *Mediafs) ReadDir(path string) (ffs.Dir, error) {
-	if err := fs.Check(); err != nil {
-		log.Println(err)
-		return nil, err
-	}
+	fs.RLock()
+	defer fs.RUnlock()
 	switch path {
 	case "/":
 		return fs.Root.Dup(), nil
@@ -134,10 +146,8 @@ func (fs *Mediafs) ReadDir(path string) (ffs.Dir, error) {
 }
 
 func (fs *Mediafs) Open(file string, mode int) (interface{}, error) {
-	if err := fs.Check(); err != nil {
-		log.Println(err)
-		return nil, err
-	}
+	fs.RLock()
+	defer fs.RUnlock()
 	switch file {
 	case "/index.html":
 		return fs.homepage.Dup(), nil
@@ -157,52 +167,3 @@ func (fs *Mediafs) Open(file string, mode int) (interface{}, error) {
 		}
 	}
 }
-
-const homepagetemplate = `
-<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		<title>mediafs</title>
-	</head>
-	<body style="background-color:#777777">
-			<div class="container card-columns">
-			{{range $ani := .DB.Anime}}
-				<div class="card" style="background-color:#FFFFEA">
-					<img class="card-img-top" src="https://img7-us.anidb.net/pics/anime/{{$ani.Picture}}" alt="{{$ani.Name}}" style="width:%10">
-					<div class="card-body">
-						<h5 class="card-title">{{$ani.Name}}</h5>
-						<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#Modal{{$ani.ID}}">Episodes</a>
-						<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#Modal{{$ani.ID}}Desc">Synopsis</a>
-					</div>
-				</div>
-				<div class="modal fade" id="Modal{{.ID}}Desc" tabindex="-1" role="dialog" aria-labelledby="Modal{{.ID}}Desc" aria-hidden="true">
-					<div class="modal-dialog modal-content modal-body">
-					<center>
-					<p>{{$ani.Description}}</p>
-					</center>
-					</div>
-				</div>
-				<div class="modal fade" id="Modal{{.ID}}" tabindex="-1" role="dialog" aria-labelledby="Modal{{.ID}}Label" aria-hidden="true">
-					<div class="modal-dialog modal-content modal-body">
-						<center>
-						<div class="list-group">
-							{{- with files $ani.Name -}}
-							{{- range . -}}
-							<a href="/{{$ani.Name}}/{{.Name}}" class="list-group-item list-group-item-action">{{.Name}}</a>
-							{{- end -}}
-							{{- end -}}
-						</div>
-						</center>
-					</div>
-				</div>
-			{{end}}
-			</div>
-	</body>
-</html>
-`
---
+++ fs/mediafs/page.go
@@ -0,0 +1,50 @@
+package mediafs
+
+const homepagetemplate = `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<title>mediafs</title>
+	</head>
+	<body style="background-color:#777777">
+			<div class="container card-columns">
+			{{range $ani := .DB.Anime}}
+				<div class="card" style="background-color:#FFFFEA">
+					<img class="card-img-top" src="https://img7-us.anidb.net/pics/anime/{{$ani.Picture}}" alt="{{$ani.Name}}" style="width:%10">
+					<div class="card-body">
+						<h5 class="card-title">{{$ani.Name}}</h5>
+						<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#Modal{{$ani.ID}}">Episodes</a>
+						<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#Modal{{$ani.ID}}Desc">Synopsis</a>
+					</div>
+				</div>
+				<div class="modal fade" id="Modal{{.ID}}Desc" tabindex="-1" role="dialog" aria-labelledby="Modal{{.ID}}Desc" aria-hidden="true">
+					<div class="modal-dialog modal-content modal-body">
+					<center>
+					<p>{{$ani.Description}}</p>
+					</center>
+					</div>
+				</div>
+				<div class="modal fade" id="Modal{{.ID}}" tabindex="-1" role="dialog" aria-labelledby="Modal{{.ID}}Label" aria-hidden="true">
+					<div class="modal-dialog modal-content modal-body">
+						<center>
+						<div class="list-group">
+							{{- with files $ani.Name -}}
+							{{- range . -}}
+							<a href="/{{$ani.Name}}/{{.Name}}" class="list-group-item list-group-item-action">{{.Name}}</a>
+							{{- end -}}
+							{{- end -}}
+						</div>
+						</center>
+					</div>
+				</div>
+			{{end}}
+			</div>
+	</body>
+</html>
+`
