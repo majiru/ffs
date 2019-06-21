816bad3591829043f941b118ff3b171c21461535
Jacob Moody <jsmoody@iastate.edu>
2019-06-21 00:11:57 -0500

    Moved some config line options and hard coded config to json file

---
+++ cmd/ffs/conf.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/majiru/ffs"
+	"github.com/majiru/ffs/fs/diskfs"
+	"github.com/majiru/ffs/fs/mkvfs"
+	"github.com/majiru/ffs/fs/domainfs"
+	"github.com/majiru/ffs/fs/mediafs"
+	"github.com/majiru/ffs/fs/pastefs"
+	"github.com/majiru/ffs/fs/jukeboxfs"
+)
+
+type FSConf struct {
+	Name string
+	SubDom string
+	Args []string
+	fs ffs.Fs
+}
+
+type Config struct {
+	Base *FSConf
+	Doms []string
+	FS []*FSConf
+}
+
+func genDefaultConf(f io.WriteSeeker) error {
+	webfs := &FSConf{"webfs", "www", []string{"./www"}, nil}
+	conf := Config{
+		webfs,
+		[]string{"localhost", "example.com"},
+		[]*FSConf{
+			webfs,
+			&FSConf{"pastefs", "paste", []string{}, nil},
+		},
+	}
+
+	json, err := json.MarshalIndent(conf, "", "\t")
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(json)
+	if err != nil {
+		return err
+	}
+	f.Seek(0, io.SeekStart)
+	return err
+}
+
+func parseFSConf(c *FSConf) error {
+	var err error
+
+	switch c.Name {
+	case "webfs":
+		c.fs = &diskfs.Diskfs{c.Args[0]}
+	case "pastefs":
+		c.fs = pastefs.NewPastefs()
+	case "mediafs":
+		if len(c.Args) > 0 {
+			f, err := os.Open(c.Args[1])
+			if err != nil {
+				return err
+			}
+			c.fs, err = mediafs.NewMediafs(f)
+			return err
+		}
+		c.fs, err = mediafs.NewMediafs(nil)
+		return err
+	case "mkvfs":
+		c.fs = mkvfs.NewMKVfs()
+	case "jukefs", "jukeboxfs":
+		if len(c.Args) != 1 {
+			return errors.New("parseFSConf: Not enough/Too many args to jukeboxfs")
+		}
+		c.fs, err = jukeboxfs.NewJukefs(c.Args[1])
+	default:
+		return errors.New("parseFSConf: Unknown fs")
+	}
+	return nil
+}
+
+func readConf(confFile io.ReadSeeker) (*Config, error) {
+	b, err := ioutil.ReadAll(confFile)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &Config{}
+	if err = json.Unmarshal(b, conf); err != nil {
+		return nil, err
+	}
+
+	if err = parseFSConf(conf.Base); err != nil {
+		return nil, err
+	}
+	for i := range conf.FS {
+		if err = parseFSConf(conf.FS[i]); err != nil {
+			return nil, err
+		}
+	}
+	return conf, nil
+}
+
+func conf2Domfs(conf *Config) *domainfs.Domainfs {
+	domfs := domainfs.NewDomainfs()
+	domfs.AddSub(conf.Base.fs, "www")
+	domfs.AddDNS(conf.Base.fs, conf.Doms...)
+	for _, fsc := range conf.FS {
+		domfs.AddSub(fsc.fs, fsc.SubDom)
+	}
+	return domfs
+}
--- cmd/ffs/main.go
+++ cmd/ffs/main.go
@@ -6,42 +6,41 @@ import (
 	"os"
 
 	"aqwari.net/net/styx"
-	"github.com/majiru/ffs/fs/diskfs"
-	"github.com/majiru/ffs/fs/domainfs"
-	"github.com/majiru/ffs/fs/mediafs"
-	"github.com/majiru/ffs/fs/pastefs"
 	"github.com/majiru/ffs/pkg/server"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage %s: http_port https_port 9p_port [domains...]\n", os.Args[0])
+	if len(os.Args) < 5 {
+		fmt.Printf("Usage %s: http_port https_port 9p_port config_file\n", os.Args[0])
 		os.Exit(1)
 	}
 	porthttp := ":" + os.Args[1]
 	porthttps := ":" + os.Args[2]
 	port9p := ":" + os.Args[3]
-	os.Args = os.Args[4:]
 
 	var styxServer styx.Server
 	styxServer.TraceLog = log.New(os.Stderr, "", 0)
 	styxServer.ErrorLog = log.New(os.Stderr, "", 0)
 
-	mfs, err := mediafs.NewMediafs(nil)
+	var f *os.File
+	var err error
+	if f, err = os.Open(os.Args[4]); err != nil {
+		log.Println(os.Args[4], "not found, creating default one")
+		f, err = os.Create(os.Args[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err = genDefaultConf(f); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	conf, err := readConf(f)
 	if err != nil {
-		log.Fatal("Could not create mfs:", err)
+		log.Fatal(err)
 	}
-	dfs := &diskfs.Diskfs{"./www"}
-	pfs := pastefs.NewPastefs()
-	domfs := domainfs.NewDomainfs()
-	domfs.AddSub(dfs, "www")
-	if len(os.Args) > 0 {
-		domfs.AddDNS(dfs, os.Args...)
-	} else {
-		domfs.AddDNS(dfs, "localhost", "example.com")
-	}
-	domfs.AddSub(pfs, "paste")
-	domfs.AddSub(mfs, "media")
+
+	domfs := conf2Domfs(conf)
 
 	srv := server.Server{domfs}
 	styxServer.Handler = styx.HandlerFunc(srv.Serve9P)
