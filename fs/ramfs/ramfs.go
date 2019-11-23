8163a0f817441d40504aa0c744a4feaab3431a74
Jacob <jsmoody@iastate.edu>
2019-11-23 15:11:43 -0600

    Touch up error triggers

--- fs/ramfs/ramfs.go
+++ fs/ramfs/ramfs.go
@@ -19,18 +19,15 @@ type Ramfs struct {
 var DirExists = errors.New("File exists already as dir")
 var FileExists = errors.New("Dir exists already as file")
 
-func (r *Ramfs) FindOrCreate(path string, isDir bool) (ffs.File, ffs.Dir, error) {
+func (r *Ramfs) FindOrCreate(file string, isDir bool) (ffs.File, ffs.Dir, error) {
 	r.Lock()
 	defer r.Unlock()
 	dir := r.Root
-	parts := strings.Split(path, "/")
+	parts := strings.Split(path.Clean(file), "/")
 	if len(parts) > 1 {
-		for i, p := range parts[1 : len(parts)-1] {
-			if p == "" {
-				continue
-			}
+		for _, p := range parts[1 : len(parts)-1] {
 			if d, err := dir.Find(p); err == nil {
-				if !d.IsDir() && i < len(parts)-1 {
+				if !d.IsDir() {
 					return nil, nil, DirExists
 				}
 				dir = d.Sys().(*fsutil.Dir)
@@ -79,8 +76,8 @@ func (r *Ramfs) ReadDir(path string) (ffs.Dir, error) {
 func (r *Ramfs) Stat(file string) (os.FileInfo, error) {
 	//If we are stating something that doesn't exist we assume a file
 	fi, err := r.Root.Walk(file)
-	if err == nil {
-		return fi, nil
+	if err != nil {
+		return nil, err
 	}
-	return fsutil.CreateFile([]byte{}, 0644, path.Base(file)).Stats, nil
+	return fi, nil
 }
