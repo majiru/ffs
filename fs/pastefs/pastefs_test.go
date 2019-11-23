809a0e510baec08163ddb5b3ece811b7a90077a0
Jacob <jsmoody@iastate.edu>
2019-11-23 15:12:39 -0600

    Increate pastefs test coverage

--- fs/pastefs/pastefs_test.go
+++ fs/pastefs/pastefs_test.go
@@ -17,8 +17,8 @@ import (
 	"github.com/majiru/ffs/pkg/server"
 )
 
-func fetchHomepage(t *testing.T, URL string) []byte {
-	homepage := fmt.Sprintf("%s/index.html", URL)
+func fetchPage(t *testing.T, URL, page string) []byte {
+	homepage := fmt.Sprintf("%s/%s", URL, page)
 	r, err := http.Get(homepage)
 	if err != nil {
 		t.Errorf("%q", err)
@@ -108,10 +108,40 @@ func TestPastefs(t *testing.T) {
 	ts := httptest.NewServer(server.Server{fs})
 	defer ts.Close()
 
-	testHomepage(t, []os.FileInfo{}, fetchHomepage(t, ts.URL))
+	testHomepage(t, []os.FileInfo{}, fetchPage(t, ts.URL, "index.html"))
 	paste1 := fsutil.CreateFile([]byte{}, 0644, ensurePaste(t, fs, ts.URL, "hello world")).Stats
 	//Pastes are stored based on UNIX time, give it a second to change
 	<-time.After(time.Second)
 	paste2 := fsutil.CreateFile([]byte{}, 0644, ensurePaste(t, fs, ts.URL, "world hello")).Stats
-	testHomepage(t, []os.FileInfo{paste1, paste2}, fetchHomepage(t, ts.URL))
+	testHomepage(t, []os.FileInfo{paste1, paste2}, fetchPage(t, ts.URL, "index.html"))
+	b := fetchPage(t, ts.URL, "new")
+	if bytes.Compare([]byte(pastepage), b) != 0 {
+		t.Fatal("content mismatch in /new page")
+	}
+}
+
+func TestReadDir(t *testing.T) {
+	fs := NewPastefs()
+	dir, err := fs.ReadDir("/")
+	if err != nil {
+		t.Fatal("error opening root dir:", err)
+	}
+	fi, err := dir.Stat()
+	if err != nil {
+		t.Fatal("error stating root dir:", err)
+	}
+	if fi.Name() != "/" {
+		t.Fatal("content mismatch for root")
+	}
+	dir, err = fs.ReadDir("/pastes")
+	if err != nil {
+		t.Fatal("error opening pastes dir:", err)
+	}
+	fi, err = dir.Stat()
+	if err != nil {
+		t.Fatal("error stating pastes dir:", err)
+	}
+	if fi.Name() != "pastes" {
+		t.Fatal("content mismatch for pastes")
+	}
 }
