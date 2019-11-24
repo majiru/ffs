d22519fb469ed011b7993bc029f272ba8a75d0d9
Jacob <jsmoody@iastate.edu>
2019-11-23 19:25:33 -0600

    Reduce duplicate code in tests

--- pkg/fsutil/file_test.go
+++ pkg/fsutil/file_test.go
@@ -1,27 +1,71 @@
 package fsutil
 
 import (
+	"bytes"
 	"io"
 	"os"
 	"sync"
 	"testing"
 )
 
+func eRead(t *testing.T, f *File, b []byte, expect []byte) {
+	n, err := f.Read(b)
+	if err != nil {
+		t.Fatal("err during read:", err)
+	}
+	if n != len(b) {
+		t.Fatal("short read")
+	}
+	if len(expect) == 0 {
+		return
+	}
+	if bytes.Compare(b, expect) != 0 {
+		t.Fatalf("expected %s got %s", string(b), string(expect))
+	}
+}
+
+func eWrite(t *testing.T, f *File, b []byte) {
+	n, err := f.Write(b)
+	if err != nil {
+		t.Fatal("err during write:", err)
+	}
+	if n != len(b) {
+		t.Fatal("short write")
+	}
+}
+
+func eReadAt(t *testing.T, f *File, b []byte, whence int64, expect []byte) {
+	n, err := f.ReadAt(b, whence)
+	if err != nil {
+		t.Fatal("err during read:", err)
+	}
+	if n != len(b) {
+		t.Fatal("short read")
+	}
+	if len(expect) == 0 {
+		return
+	}
+	if bytes.Compare(b, expect) != 0 {
+		t.Fatalf("expected %s got %s", string(b), string(expect))
+	}
+}
+
+func eWriteAt(t *testing.T, f *File, b []byte, whence int64) {
+	n, err := f.WriteAt(b, whence)
+	if err != nil {
+		t.Fatal("err during write:", err)
+	}
+	if n != len(b) {
+		t.Fatal("short write")
+	}
+}
+
 func TestRead(t *testing.T) {
 	buf := make([]byte, 1)
-	testStr := "Testing"
-	f := CreateFile([]byte(testStr), 0644, "test")
+	testStr := []byte("Testing")
+	f := CreateFile(testStr, 0644, "test")
 	for i := range testStr {
-		n, err := f.Read(buf)
-		if err != nil {
-			t.Errorf("Read returned err: %v", err)
-		}
-		if n != 1 {
-			t.Errorf("Read returned %d for 1 byte read", n)
-		}
-		if buf[0] != testStr[i] {
-			t.Errorf("Read retuned %v, expected %v", buf[0], testStr[i])
-		}
+		eRead(t, f, buf, []byte{testStr[i]})
 	}
 }
 
@@ -30,67 +74,35 @@ func TestReadConcur(t *testing.T) {
 	testStr := "Testing"
 	f := CreateFile([]byte(testStr), 0644, "test")
 	for range testStr {
-		go func() {
-			n, err := f.Read(buf)
-			if err != nil {
-				t.Errorf("Read returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("Read returned %d for 1 byte read", n)
-			}
-		}()
+		go eRead(t, f, buf, []byte{})
 	}
 }
 
 func TestReadDup(t *testing.T) {
 	buf := make([]byte, 1)
-	testStr := "Testing"
-	f := CreateFile([]byte(testStr), 0644, "test")
+	testStr := []byte("Testing")
+	f := CreateFile(testStr, 0644, "test")
 	for range testStr {
 		go func() {
-			n, err := f.Dup().Read(buf)
-			if err != nil {
-				t.Errorf("Read returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("Read returned %d for 1 byte read", n)
-			}
-			if buf[0] != testStr[0] {
-				t.Errorf("Read retuned %v, expected %v", buf[0], testStr[0])
-			}
+			eRead(t, f.Dup(), buf, []byte{testStr[0]})
 		}()
 	}
 }
 
 func TestWrite(t *testing.T) {
 	testStr := []byte("Testing")
-	f := CreateFile([]byte(""), 0644, "test")
-	n, err := f.Write(testStr)
-	if err != nil {
-		t.Errorf("Write returned err: %v", err)
-	}
-	if n != len(testStr) {
-		t.Errorf("Did write all of buffer")
-	}
+	f := CreateFile([]byte{}, 0644, "test")
+	eWrite(t, f, testStr)
 	if f.Size() != int64(len(testStr)) {
 		t.Errorf("Size does not properly reflect buffer")
 	}
-	if _, err = f.Seek(0, io.SeekStart); err != nil {
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
 		t.Errorf("Got err %v when seeking to start", err)
 	}
 
 	buf := make([]byte, 1)
 	for i := range testStr {
-		n, err := f.Read(buf)
-		if err != nil {
-			t.Errorf("Read returned err: %v", err)
-		}
-		if n != 1 {
-			t.Errorf("Read returned %d for 1 byte read", n)
-		}
-		if buf[0] != testStr[i] {
-			t.Errorf("Read retuned %v, expected %v", buf[0], testStr[i])
-		}
+		eRead(t, f, buf, []byte{testStr[i]})
 	}
 }
 
@@ -101,13 +113,7 @@ func TestWriteConcur(t *testing.T) {
 	wg.Add(len(testStr))
 	for i := range testStr {
 		go func() {
-			n, err := f.Write([]byte{testStr[i]})
-			if err != nil {
-				t.Errorf("Write returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("Write returned %d written for 1 byte write", n)
-			}
+			eWrite(t, f, []byte{testStr[i]})
 			wg.Done()
 		}()
 	}
@@ -118,41 +124,19 @@ func TestWriteConcur(t *testing.T) {
 }
 
 func TestReadAt(t *testing.T) {
-	testStr := "Testing"
-	f := CreateFile([]byte(testStr), 0644, "test")
+	testStr := []byte("Testing")
+	f := CreateFile(testStr, 0644, "test")
 	wg := sync.WaitGroup{}
 	wg.Add(20)
 	for i := 0; i < 10; i++ {
 		go func() {
 			buf := make([]byte, 1)
-			n, err := f.ReadAt(buf, 0)
-			if err != nil {
-				t.Errorf("ReadAt returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("ReadAt returned %d for 1 byte read", n)
-			}
-			if string(buf)[0] != testStr[0] {
-				t.Errorf("ReadAt did not grab the right content. Expected %c, got %c", string(buf)[0], testStr[0])
-			}
+			eReadAt(t, f, buf, 0, []byte{testStr[0]})
 			wg.Done()
 		}()
 		go func() {
 			buf := make([]byte, 1)
-			df := f.Dup()
-			if _, err := df.Seek(1, io.SeekStart); err != nil {
-				t.Errorf("Seek returned err: %v", err)
-			}
-			n, err := f.ReadAt(buf, 1)
-			if err != nil {
-				t.Errorf("ReadAt returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("ReadAt returned %d for 1 byte read", n)
-			}
-			if string(buf)[0] != testStr[1] {
-				t.Errorf("ReadAt did not grab the right content. Expected %v, got %v", string(buf)[0], testStr[1])
-			}
+			eReadAt(t, f, buf, 1, []byte{testStr[1]})
 			wg.Done()
 		}()
 	}
@@ -161,29 +145,13 @@ func TestReadAt(t *testing.T) {
 
 func TestWriteAt(t *testing.T) {
 	testStr := []byte("Testing")
-	f := CreateFile([]byte(testStr), 0644, "test")
+	f := CreateFile(testStr, 0644, "test")
 	wg := sync.WaitGroup{}
 	wg.Add(len(testStr))
 	for i := range testStr {
 		go func() {
-			n, err := f.WriteAt([]byte{testStr[i]}, 1)
-			if err != nil {
-				t.Errorf("Write returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("Write returned %d written for 1 byte write", n)
-			}
-			buf := make([]byte, 1)
-			n, err = f.ReadAt(buf, int64(i))
-			if err != nil {
-				t.Errorf("ReadAt returned err: %v", err)
-			}
-			if n != 1 {
-				t.Errorf("ReadAt returned %d for 1 byte read", n)
-			}
-			if string(buf)[0] != testStr[i] {
-				t.Errorf("ReadAt did not grab the right content. Expected %v, got %v", string(buf)[0], testStr[i])
-			}
+			eWriteAt(t, f, []byte{testStr[i]}, 1)
+			eReadAt(t, f, make([]byte, 1), int64(i), []byte{testStr[i]})
 			wg.Done()
 		}()
 	}
@@ -198,14 +166,7 @@ func TestTruncate(t *testing.T) {
 	if err != nil {
 		t.Fatal("Truncate returned error:", err)
 	}
-	b := make([]byte, 4)
-	n, err := f.ReadAt(b, 0)
-	if n != 4 {
-		t.Fatal("Truncate: short read")
-	}
-	if string(b) != "Testing"[:4] {
-		t.Fatal("Truncate: content mismatch")
-	}
+	eReadAt(t, f, make([]byte, 4), 0, testStr[:4])
 	err = f.Truncate(10)
 	if err != nil {
 		t.Fatal("Truncate returned error:", err)
@@ -221,31 +182,21 @@ func TestSeekPos(t *testing.T) {
 	if f.SeekPos() != 0 {
 		t.Fatal("SeekPos is not 0 at creation of file")
 	}
+
+	seekErr := func(f *File, expect int) {
+		if f.SeekPos() != int64(expect) {
+			t.Fatalf("expected %d got %d for SeekPos after read", expect, f.SeekPos())
+		}
+	}
+
+	readSeek := func(f *File, b []byte, expect int) {
+		eRead(t, f, b, []byte{})
+		seekErr(f, expect)
+	}
 	b := make([]byte, 3)
-	n, err := f.Read(b)
-	if err != nil {
-		t.Fatal("Read returned err:", err)
-	}
-	if n != 3 {
-		t.Fatal("short read")
-	}
-	if f.SeekPos() != 3 {
-		t.Fatalf("expected %d got %d for SeekPos after read", 3, f.SeekPos())
-	}
-	f2 := f.Dup()
-	n, err = f2.Read(b)
-	if err != nil {
-		t.Fatal("Read returned err:", err)
-	}
-	if n != 3 {
-		t.Fatal("short read")
-	}
-	if f2.SeekPos() != 6 {
-		t.Fatalf("expected %d got %d for SeekPos after read", 6, f.SeekPos())
-	}
-	if f.SeekPos() != 3 {
-		t.Fatalf("expected %d got %d for SeekPos after read", 3, f.SeekPos())
-	}
+	readSeek(f, b, 3)
+	readSeek(f.Dup(), b, 6)
+	seekErr(f, 3)
 }
 
 func TestClose(t *testing.T) {
@@ -254,16 +205,9 @@ func TestClose(t *testing.T) {
 	if f.SeekPos() != 0 {
 		t.Fatal("SeekPos is not 0 at creation of file")
 	}
-	b := make([]byte, 3)
-	n, err := f.Read(b)
-	if err != nil {
-		t.Fatal("Read returned err:", err)
-	}
-	if n != 3 {
-		t.Fatal("short read")
-	}
+	eRead(t, f, make([]byte, 3), testStr[:3])
 	f2 := f.Dup()
-	err = f.Close()
+	err := f.Close()
 	if err != nil {
 		t.Fatal("Error returned err:", err)
 	}
@@ -323,41 +267,32 @@ func TestSeek(t *testing.T) {
 func TestReadWriteEof(t *testing.T) {
 	testStr := []byte("Testing")
 	f := CreateFile(testStr, 0644, "test")
-	n, err := f.WriteAt([]byte{}, -1)
-	if err != ErrNeg {
-		t.Fatalf("expected %v got %v for negative WriteAt()", ErrNeg, err)
+	ioErrAt := func(expect error, len int, b []byte, whence int64, fun func([]byte, int64) (int, error)) {
+		n, err := fun(b, whence)
+		if err != expect {
+			t.Fatalf("expected %v got %v", expect, err)
+		}
+		if n != len {
+			t.Fatalf("expected %d got %d", len, n)
+		}
 	}
-	if n != 0 {
-		t.Fatalf("expected %d got %d for negative WriteAt()", 0, n)
+	ioErr := func(expect error, len int, b []byte, fun func([]byte) (int, error)) {
+		n, err := fun(b)
+		if err != expect {
+			t.Fatalf("expected %v got %v", expect, err)
+		}
+		if n != len {
+			t.Fatalf("expected %d got %d", len, n)
+		}
 	}
-	n, err = f.ReadAt([]byte{}, -1)
-	if err != ErrNeg {
-		t.Fatalf("expected %v got %v for negative ReadAt()", ErrNeg, err)
-	}
-	if n != 0 {
-		t.Fatalf("expected %d got %d for negative ReadAt()", 0, n)
-	}
+	ioErrAt(ErrNeg, 0, []byte{}, -1, f.WriteAt)
+	ioErrAt(ErrNeg, 0, []byte{}, -1, f.ReadAt)
+
 	b := make([]byte, len(testStr)+1)
-	n, err = f.ReadAt(b, 0)
-	if err != io.EOF {
-		t.Fatalf("expected %v got %v for EOF ReadAt()", io.EOF, err)
-	}
-	if n != len(testStr) {
-		t.Fatalf("expected %d got %d for EOF ReadAt()", len(testStr), n)
-	}
+	ioErrAt(io.EOF, len(testStr), b, 0, f.ReadAt)
+
 	f.Read(make([]byte, len(testStr)))
-	n, err = f.Read(b)
-	if err != io.EOF {
-		t.Fatalf("expected %v got %v for EOF Read()", io.EOF, err)
-	}
-	if n != 0 {
-		t.Fatalf("expected %d got %d for EOF Read()", 0, n)
-	}
-	n, err = f.ReadAt(b, int64(len(testStr)+1))
-	if err != io.EOF {
-		t.Fatalf("expected %v got %v for EOF ReadAt()", io.EOF, err)
-	}
-	if n != 0 {
-		t.Fatalf("expected %d got %d for EOF ReadAt()", 0, n)
-	}
+	ioErr(io.EOF, 0, make([]byte, len(testStr)), f.Read)
+
+	ioErrAt(io.EOF, 0, b, int64(len(testStr)+1), f.ReadAt)
 }
